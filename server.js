5a46bea1a1e6a7f2f41251787535e61ee256ad7b
Claire Davies <claire.davies@junitech.com>
2021-03-23 17:22:49 +0000

    set up express and got port listening

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3000; 
 
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
  
-app.listen(3001)
+app.listen(3000, () => console.log('listening on port 3000'));
