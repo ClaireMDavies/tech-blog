fc8fddd94eed57e8df5b9b6908845c964fbe71dc
Claire Davies <claire.davies@junitech.com>
2021-03-23 16:43:04 +0000

    dependencies installed in package.json

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001; 
+
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+ 
+app.listen(3001)
